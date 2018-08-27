9d3af130721b293a98d169a2ece5bc6614df251a
lifeprolific <thegregoman@gmail.com>
2018-08-27 13:55:30 -0400

    added several books to the database

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,11 +3,23 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
   end
 
   def edit
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to @book
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :author, :description, :url, :img_url)
   end
 end
