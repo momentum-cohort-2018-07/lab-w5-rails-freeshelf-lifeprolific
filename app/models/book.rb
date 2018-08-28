7d0cde2fd9a5cc747c49254ef7a25c00a31336f8
lifeprolific <thegregoman@gmail.com>
2018-08-28 14:31:34 -0400

    added reference and migrated

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,4 @@
 class Book < ApplicationRecord
   validates :title, :author, :description, :url,  presence: true
+  belongs_to :author
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   validates :username, uniqueness: true
   validates :username, presence: true
+  has_many :books
 end
