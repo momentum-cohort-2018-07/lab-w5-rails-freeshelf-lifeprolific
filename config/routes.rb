9f5516b1d44e7c691767d2b583d3cfa527f43974
lifeprolific <thegregoman@gmail.com>
2018-08-29 14:52:59 -0400

    refactored sessions to session

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   get 'users/register'
-  resources :sessions, only: [:new, :create, :destroy]
+  resources :session, only: [:new, :create, :destroy]
   resources :users, only: [:register, :create]
   resources :books
   
