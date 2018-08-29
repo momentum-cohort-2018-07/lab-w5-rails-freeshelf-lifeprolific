a2bfe0c2691ed327df197db195ccb0b2a4f81755
lifeprolific <thegregoman@gmail.com>
2018-08-29 13:19:59 -0400

    added user registration view and passwords

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
   def register
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params.require(:user).permit(:username, :password))
+    if @user.save
+      redirect_to books_path
+    else
+      render 'register'
+    end
   end
 end
