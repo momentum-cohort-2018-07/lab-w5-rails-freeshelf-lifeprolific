1fe242ad17dbe101be40580f0c8c8c197b9e920f
lifeprolific <thegregoman@gmail.com>
2018-08-29 12:55:42 -0400

    added password field and generated User controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_secure_password
   validates :username, uniqueness: true
   validates :username, presence: true
   has_many :books
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_28_194858) do
+ActiveRecord::Schema.define(version: 2018_08_29_165132) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2018_08_28_194858) do
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "books", "users"
