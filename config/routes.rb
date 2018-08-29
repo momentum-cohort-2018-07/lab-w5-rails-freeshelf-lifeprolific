428f4c4256728f47b7306afcb7b7a8a602c03510
lifeprolific <thegregoman@gmail.com>
2018-08-29 13:03:51 -0400

    added users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   get 'users/register'
+  resources :users, only: [:register, :create]
   resources :books
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
