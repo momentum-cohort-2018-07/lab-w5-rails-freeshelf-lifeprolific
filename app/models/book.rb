d74414766ebe0f9447995c18548914aee6e1f8b0
lifeprolific <thegregoman@gmail.com>
2018-08-29 15:28:56 -0400

    fixed checkbox validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
-  validates :title, :author, :description, :url, :checked_out, presence: true
+  validates :title, :author, :description, :url, presence: true
   validates_associated :user
   belongs_to :user
 end
